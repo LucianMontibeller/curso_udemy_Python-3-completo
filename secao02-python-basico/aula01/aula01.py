0804929dab7d4a2745f33eb3cf303961f87adefb
LucianMontibeller <luciandeborah@gmail.com>
2022-10-27 23:18:54 -0300

    add aula01

---
+++ secao02-python-basico/aula01/aula01.py
@@ -0,0 +1,21 @@
+"""
+# Comentário
+print('Linha 1') # outro comentário
+print('Linha 2')
+# Falando sobre do que se trata essa linha
+print('Linha 3')
+print('Linha 4')  # Isso é um comentário
+print('Linha 5')  # mais um comentário
+print('Linha 6')
+
+"""
+
+# Comentário
+print('Linha 1') # outro comentário
+print('Linha 2')
+# Falando sobre do que se trata essa linha
+print('Linha 3')
+print('Linha 4')  # Isso é um comentário
+print('Linha 5')  # mais um comentário
+print('Linha 6')
+
