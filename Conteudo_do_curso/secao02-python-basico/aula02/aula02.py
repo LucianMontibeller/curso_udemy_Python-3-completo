c8689b3c8d53bb8c416df03b632b7c5aac9c80df
LucianMontibeller <luciandeborah@gmail.com>
2022-10-30 22:15:29 -0300

    aula02

---
+++ Conteudo_do_curso/secao02-python-basico/aula02/aula02.py
@@ -0,0 +1,12 @@
+print(12345)
+print('Lucian', 'Montibeller', sep='-', end='=====')
+print('joão', 'e', 'maria', sep='-', end='')
+print()
+
+'''
+824.176.070-18
+'''
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
+
