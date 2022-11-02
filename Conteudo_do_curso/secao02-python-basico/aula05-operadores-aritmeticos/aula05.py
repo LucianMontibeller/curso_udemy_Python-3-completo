617fff853cba6c16d3801e3e88cad7814be2d1ed
LucianMontibeller <luciandeborah@gmail.com>
2022-11-02 11:56:36 -0300

    aula05

---
+++ Conteudo_do_curso/secao02-python-basico/aula05-operadores-aritmeticos/aula05.py
@@ -0,0 +1,19 @@
+'''
++, - , *, /, //, **, %, ()
+
+
+'''
+
+print('Multiplicação *', 10 * 10)
+print('Adição +', 10 + 10)
+print('Subtração -', 10 - 5)
+print('Divisão /', 10 / 2)
+
+print('Lucian' + ' ' + 'ele tem ' + str(33) + ' anos')
+
+print(10.5 // 3)
+print(10.5 / 3)
+print(10 % 5)
+
+print(5 + 2 * 10)
+print((5 + 2) * 10)
