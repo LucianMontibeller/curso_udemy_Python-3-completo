20b7c3fd5c5822ae3bf6fca81dff0aae6f8e7bb1
LucianMontibeller <luciandeborah@gmail.com>
2022-11-02 10:41:26 -0300

    aula04

---
+++ Conteudo_do_curso/secao02-python-basico/aula04-tipos-de-dados-primitivos/aula04.py
@@ -0,0 +1,26 @@
+'''
+Tipos de dados
+str - String -textos 'Assim'
+int - inteiro - 123456 0 -10 -20 -50
+float - real/ponto flutuante - 10.5 1.5 -10.10 0.0
+bool - booleano/lógico - True/False 10 == 10
+'''
+
+print(type('Lucian'))
+print('10', type('10'))
+print('Lucian', type('LUcian'))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
+
+
+# String: Nome
+print('Lucian', type('Lucian'))
+
+# Idade: int
+print(33, type(33))
+
+# Altura: float
+print(1.75, type(1.75))
+
+# É maior de idade 10 > 20
+print(33 > 18, type(33 > 18))
