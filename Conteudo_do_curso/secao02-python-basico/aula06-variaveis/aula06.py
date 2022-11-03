57df022dddf4cfbd730904a00a142c656c464775
LucianMontibeller <luciandeborah@gmail.com>
2022-11-03 20:36:36 -0300

    aula06

---
+++ Conteudo_do_curso/secao02-python-basico/aula06-variaveis/aula06.py
@@ -0,0 +1,15 @@
+'''
+Iniciar com letra, pode conter números, separar _, letras minúsculas.
+'''
+
+nome = 'Lucian'
+idade = 32
+altura = 1.80
+e_maior = idade > 18
+peso = 80
+
+print(nome)
+print(nome, type(nome))
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior:', e_maior)
